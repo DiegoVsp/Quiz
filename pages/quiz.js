ec4049e353739ab12c340b2970f86710c854fb32
DiegoVsp <diego-vespa2020@outlook.com.br>
2021-03-08 18:12:10 -0300

    atualizando projeto

--- pages/quiz.js
+++ pages/quiz.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
 
 import db from '../db.json';
@@ -20,27 +21,57 @@ function LoadingWidget() {
     </Widget>
   );
 }
+function QuestionWidget({ question, totalQuestions, questionIndex }) {
+  const questionId = `question__${questionIndex}`;
+  return (
+    <Widget>
+      <Widget.Header>
+        <h3>
+          {`Pergunta ${questionIndex} de ${totalQuestions}`}
+        </h3>
+      </Widget.Header>
+      <img src={question.image} alt="Descrição" style={{ width: '100%', height: '150px', objectFit: 'cover' }} />
+      <Widget.Content>
+        <h2>{question.title}</h2>
+        <p>{question.description}</p>
+        <form action="">
+          {question.alternatives.map((alternative, alternativeIndex) => {
+            const alternativeID = `alternative_${alternativeIndex}`;
+            return (
+              <Widget.Topic as="label" htmlFor={alternativeID}>
+                <input
+                  // style={{ display: 'none' }}
+                  id={alternativeID}
+                  name={questionId}
+                  type="radio"
+                />
+                {alternative}
+              </Widget.Topic>
+            );
+          })}
+
+          <Button type="submit">Confirmar</Button>
+        </form>
+      </Widget.Content>
+    </Widget>
+  );
+}
 
 export default function QuizPage() {
-  const questions = db.questions[0];
+  const questionIndex = 1;
+  const question = db.questions[questionIndex];
+  const totalQuestions = db.questions.length;
+
   console.log('Perguntas criadas', db.questions);
   return (
     <QuizBackground backgroundImage={db.bg}>
       <QuizContainer>
         <QuizLogo />
-        <Widget>
-          <Widget.Header>
-            <h3>
-              {`Pergunta 1 de ${db.questions.length}`}
-            </h3>
-          </Widget.Header>
-          <img src={questions.image} alt="Descrição" style={{ width: '100%', height: '150px', objectFit: 'cover' }} />
-          <Widget.Content>
-            <h2>{questions.title}</h2>
-            <p>{questions.description}</p>
-            <Button>Confirmar</Button>
-          </Widget.Content>
-        </Widget>
+        <QuestionWidget
+          question={question}
+          totalQuestions={totalQuestions}
+          questionIndex={questionIndex}
+        />
         <LoadingWidget />
 
       </QuizContainer>
--- src/components/Widget/index.js
+++ src/components/Widget/index.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import db from '../../../db.json';
 
 const Widget = styled.div`
 margin-top:24px;
@@ -45,6 +46,27 @@ ul {
   list-style:none;
   padding:0;
 }
+`;
+
+Widget.Topic = styled.a`
+  outline: 0;
+  text-decoration: none;
+  color: ${({ theme }) => theme.colors.contrastText};
+  background: ${(props) => props.bg || `${db.theme.colors.primary}70`};
+  padding: 10px 15px;
+  margin-bottom: 8px;
+  cursor: pointer;
+  border-radius: ${({ theme }) => theme.borderRadius};
+  transition: .3s;
+  display: block;
+  font-size: 15px;
+  font-weight: 400;
+  letter-spacing: 1px;
+  box-shadow: ${({ theme }) => theme.boxShadow};
+  &:hover,
+  &:focus {
+    background-color: ${({ theme }) => `${theme.colors.extraColor}60`};
+  }
 `;
 
 export default Widget;
